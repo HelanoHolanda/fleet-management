a050d4156ad6d280b8957c073d8687a15323fcb3
HelanoHolanda <holandahelano02@gmail.com>
2026-06-05 13:54:16 -0300

    feat: setup and paste implementation

---
+++ src/app.module.ts
@@ -0,0 +1,49 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CacheModule } from '@nestjs/cache-manager';
+import { createKeyv } from '@keyv/redis';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mssql',
+        host: config.get('DB_HOST'),
+        port: parseInt(config.get('DB_PORT') ?? '1433'),
+        username: config.get('DB_USER'),
+        password: config.get('DB_PASS'),
+        database: config.get('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        migrations: [__dirname + '/migrations/*{.ts,.js}'],
+        synchronize: false,
+        options: {
+          encrypt: false,
+          trustServerCertificate: true,
+        },
+      }),
+    }),
+
+    CacheModule.registerAsync({
+      isGlobal: true,
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        stores: [
+          createKeyv(
+            `redis://${config.get('REDIS_HOST')}:${config.get('REDIS_PORT')}`,
+          ),
+        ],
+        ttl: +config.get('CACHE_TTL') * 1000,
+      }),
+    }),
+  ],
+})
+export class AppModule {}
